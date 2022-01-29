99af6559f8200133d2eb67e1737ae08ebff01933
prithviraj <prithvi2410rj@gmail.com>
2022-01-29 10:04:25 +0530

    worked on create page

--- Create.js
+++ Create.js
@@ -1,12 +1,7 @@
-
-document.getElementById('submit').addEventListener('submit',validateName);
-
 function validateName(e)
 {
-    const name =document.getElementById('Name');
-    console.log(name);
-    const len=name.length;
-    if(len<=0)
+    const name =document.getElementById('Name').value;
+    if(name.length<=0)
     {
         e.preventDefault();
         document.getElementById('name-error').innerText="name cannot be empty";
@@ -14,11 +9,90 @@ function validateName(e)
     }
     else{
         document.getElementById('name-error').innerText="";
-        document.getElementById('name-error').style.margin=1.5; 
+        //document.getElementById('name-error').style.color=white;
+        //document.getElementById('name-error').style.margin=(window.innerHeight * 0.4); 
     }
+}
+function validateEmail(e)
+{
+    const email = document.getElementById('Email').value;
+    if(email.length==0)
+    {
+        e.preventDefault();
+        document.getElementById('email-error').innerText="email cannot be empty";
+        document.getElementById('email-error').style.margin=0; 
+    }
+    else if(email.includes("@") == false || email.includes(".") == false )
+    {
+        e.preventDefault();
+        document.getElementById('email-error').innerText="invalid email format";
+        document.getElementById('email-error').style.margin=0; 
+    }
+    else{
+        document.getElementById('email-error').innerText="  "; 
+    }
+}
+function validateMobileNumber(e){
+    const mobile = document.getElementById('MobileNumber').value;
+    if(mobile.length==0)
+    {
+        e.preventDefault();
+        document.getElementById('mobile-number-error').innerText="mobile number cannot be empty";
+        document.getElementById('mobile-number-error').style.margin=0; 
+    }
+    else if(mobile.length!=10 || mobile.charAt(0)<6)
+    {
+        e.preventDefault();
+        document.getElementById('mobile-number-error').innerText="invalid mobile number";
+        document.getElementById('mobile-number-error').style.margin=0; 
+    }
+    else{
+        document.getElementById('mobile-number-error').innerText="  "; 
+    }
+}
+function validateUsername(e){
+    const username = document.getElementById('Username').value;
+    if(username.length==0)
+    {
+        e.preventDefault();
+        document.getElementById('username-error').innerText="username cannot be empty";
+        document.getElementById('username-error').style.margin=0; 
+    }
+    else if(username.length<8)
+    {
+        e.preventDefault();
+        document.getElementById('username-error').innerText="username should be minimum of 8 characters";
+        document.getElementById('username-error').style.margin=0; 
+    }
+    else{
+        document.getElementById('username-error').innerText="  "; 
+    }   
+}
+function validatePassword(e){
+    const password = document.getElementById('Password').value;
+    if(password.length==0)
+    {
+        e.preventDefault();
+        document.getElementById('password-error').innerText="password cannot be empty";
+        document.getElementById('password-error').style.margin=0; 
+    }
+    else if(password.length<8)
+    {
+        e.preventDefault();
+        document.getElementById('password-error').innerText="password should be minimum of 8 characters";
+        document.getElementById('password-error').style.margin=0; 
+    }
+    else{
+        document.getElementById('password-error').innerText="  "; 
+    }   
 }
 
 
+document.getElementById('submit').addEventListener('click',validateName);
+document.getElementById('submit').addEventListener('click',validateEmail);
+document.getElementById('submit').addEventListener('click',validateMobileNumber);
+document.getElementById('submit').addEventListener('click',validateUsername);
+document.getElementById('submit').addEventListener('click',validatePassword);
 
 
 
@@ -48,22 +122,14 @@ function validateName(e)
 
 
 
-// document.getElementsByClassName('submit-btn')[0].addEventListener('click',validateEmail);
+
 // document.getElementsByClassName('submit-btn')[0].addEventListener('click',validateMoblie);
 // document.getElementsByClassName('submit-btn')[0].addEventListener('click',validateUsername);
 // document.getElementsByClassName('submit-btn')[0].addEventListener('click',validatePassword);
 
 
 
-// function validateEmail(e)
-// {
-//     if(document.getElementById('#Email').Value==null)
-//     {
-//         e.preventDefault();
-//         document.getElementById('email-error').innerText="email cannot be empty";
-//         document.getElementById('email-error').style.margin=0; 
-//     }
-// }
+
 // function validateMoblie(e)
 // {
 //     if(document.getElementById('#MobileNumber').Value==null)
